8b4b241dec248e1ae69c3aee0ba2797f115b9875
Samir Landou <samirlandou@gmail.com>
2020-10-28 22:50:25 -0300

    Implementing @ManyToOne in Restaurante domain model

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,6 +25,9 @@ public class Restaurante {
 	private String nome;
 	
 	@Column(name = "taxa_frete")
-	private BigDecimal taxaFrete;	
+	private BigDecimal taxaFrete;
+	
+	@ManyToOne
+	private Cozinha cozinha;
 	
 }
