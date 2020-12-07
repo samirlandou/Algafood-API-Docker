2c15f8d4b8422ab7d100777d8b3a0a479c117b96
Samir Landou <samirlandou@gmail.com>
2020-12-06 22:27:59 -0300

    Modeling and Implementing Atualization of Restaurante Resource

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -40,10 +39,13 @@ public class CozinhaController {
 		return cozinhaRepository.listar();		
 	}
 	
-	@GetMapping(produces = 	MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml(){
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());		
-	}
+	
+	/*
+	 * para tratar o methodo com o xml
+	 * @GetMapping(produces = MediaType.APPLICATION_XML_VALUE) public
+	 * CozinhasXmlWrapper listarXml(){ return new
+	 * CozinhasXmlWrapper(cozinhaRepository.listar()); }
+	 */
 	
 	
 	/*
@@ -99,7 +101,7 @@ public class CozinhaController {
 			 //cozinhaAtual.setNome(cozinha.getNome());
 			 BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
 			 
-			 cozinhaRepository.salvar(cozinhaAtual);
+			 cozinhaAtual = cadastroCozinhaService.salvar(cozinhaAtual);
 			 
 			 return ResponseEntity.ok(cozinhaAtual);			 
 		 }
--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,4 +58,26 @@ public class RestauranteController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
     }
+    
+    
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+
+		try {
+			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+			if (restauranteAtual != null) {
+				// cozinhaAtual.setNome(cozinha.getNome());
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+				restauranteAtual = cadastroRestauranteService.salvar(restauranteAtual);
+
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			// return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			return ResponseEntity.notFound().build();
+
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 }
